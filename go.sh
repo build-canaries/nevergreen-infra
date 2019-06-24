f8221c1022ec867fe974558d0aae342c18f5ebde
Christopher Martin <eternalepoch@gmail.com>
2019-06-24 02:22:22 +0100

    Install zulu openjdk 12

--- go.sh
+++ go.sh
@@ -30,8 +30,12 @@ function create_deployment_user {
 }
 
 function install_java_and_haproxy {
+    ssh $REMOTE "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9"
+    ssh $REMOTE "sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'"
     ssh $REMOTE "sudo apt-get update"
-    ssh $REMOTE "sudo apt-get install haproxy default-jre -y" # need to make this explicit about java8
+    ssh $REMOTE "sudo apt-get install zulu-12 -y"
+
+    ssh $REMOTE "sudo apt-get install haproxy -y"
 }
 
 function setup_haproxy {
