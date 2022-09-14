ea85b57990e3e2316c7d2a42b458be7b41f757bf
joe.wright <joe@joejag.com>
2022-09-14 17:54:14 +0100

    do not concat in a subshell in case this is causing an issue

--- update-certs.sh
+++ update-certs.sh
@@ -4,7 +4,7 @@
 certbot renew --force-renewal
 
 # Concatenate new cert files
-bash -c "cat /etc/letsencrypt/live/nevergreen/fullchain.pem /etc/letsencrypt/live/nevergreen/privkey.pem > /etc/haproxy/certs/nevergreen.io.pem"
+cat /etc/letsencrypt/live/nevergreen/fullchain.pem /etc/letsencrypt/live/nevergreen/privkey.pem > /etc/haproxy/certs/nevergreen.io.pem
 
 # Reload HAProxy
 service haproxy reload
