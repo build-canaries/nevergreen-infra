f37040b346b3e44c56ecedd90fe5c94bcba6a4e9
joe.wright <joe@joejag.com>
2023-05-16 22:32:45 +0100

    cronjobs need full PATH to run

--- update-certs.sh
+++ update-certs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+date
+
 # Renew the certificate
 certbot renew --force-renewal
 
@@ -7,4 +9,4 @@ certbot renew --force-renewal
 cat /etc/letsencrypt/live/nevergreen/fullchain.pem /etc/letsencrypt/live/nevergreen/privkey.pem > /etc/haproxy/certs/nevergreen.io.pem
 
 # Reload HAProxy
-serivce haproxy restart
+/usr/sbin/service haproxy restart
