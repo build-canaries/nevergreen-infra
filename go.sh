ee590ab803955e286c6cff88afeb673ef18d502e
Christopher Martin <GentlemanHal@users.noreply.github.com>
2023-06-29 23:23:10 +0100

    build: updates jdk version
    Refs: #13

--- go.sh
+++ go.sh
@@ -33,7 +33,7 @@ function install_java_and_haproxy {
     ssh $REMOTE "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9"
     ssh $REMOTE "sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'"
     ssh $REMOTE "sudo apt-get update"
-    ssh $REMOTE "sudo apt-get install zulu-13 -y"
+    ssh $REMOTE "sudo apt-get install zulu19 -y"
 
     ssh $REMOTE "sudo apt-get install haproxy -y"
 }
